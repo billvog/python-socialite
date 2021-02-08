b4ed87fac6a7907ab8d73e3bfb2f63bee746ed06
Evans Mwendwa <evans@authenticvisualsmedia.com>
2021-02-08 17:34:36 +0300

    add facebook driver

--- src/python_socialite/drivers/facebook.py
+++ src/python_socialite/drivers/facebook.py
@@ -10,16 +10,14 @@ class FacebookProvider(AbstractDriver):
         """Initialize Google provider."""
         super().__init__(config)
         fb_scopes = ["email", "public_profile"]
-        self.version = "v9.0"
-        self.graph_url = "https://graph.facebook.com"
         self.scopes = config.get("scopes", fb_scopes)
 
     def get_auth_url(self, state=None):
-        url = f"https://www.facebook.com/{self.version}/dialog/oauth"
+        url = "https://www.facebook.com/v9.0/dialog/oauth"
         return self.build_url(url, state)
 
     def get_token_url(self):
-        return f"{self.graph_url}/{self.version}/oauth/access_token"
+        return "https://graph.facebook.com/v9.0/oauth/access_token"
 
     def get_token_fields(self, code, state=None):
         fields = super().get_token_fields(code, state)
@@ -27,7 +25,7 @@ class FacebookProvider(AbstractDriver):
         return fields
 
     def get_user_by_token(self, access_token):
-        url = f"{self.graph_url}/{self.version}/me"
+        url = "https://graph.facebook.com/v9.0/me"
         url = url + "?fields=name,email,id,picture,first_name,last_name"
         headers = {"Authorization": f"Bearer {access_token}"}
         response = requests.get(url, headers=headers)
@@ -55,7 +53,7 @@ class FacebookProvider(AbstractDriver):
 
     def map_user_to_dict(self, raw_user):
         user_id = raw_user.get("id")
-        avatar_url = f"{self.graph_url}/{self.version}/{user_id}/picture/?type=large&width=1600"
+        avatar_url = f"https://graph.facebook.com/v9.0/{user_id}/picture/?type=large&width=1600"
         user = dict(abstract_user)
         user["id"] = user_id
         user["name"] = raw_user.get("name")
---
+++ tests/test_github.py
@@ -0,0 +1,71 @@
+import pytest
+from urllib import parse
+from unittest.mock import patch
+from python_socialite.python_socialite import OAuthProvider
+from python_socialite.exceptions import BadVerification
+
+config = {
+    "github": {
+        "client_id": "client_id_1",
+        "client_secret": "***",
+        "redirect_url": "http://localhost.com",
+        "scopes": ["email", "user"],
+    },
+}
+
+
+@pytest.fixture
+def token():
+    token = {
+        "access_token": "access_token",
+        "id_token": "",
+        "token_type": "Bearer",
+        "scope": "email",
+    }
+    return token
+
+
+def test_get_auth_url():
+    provider = OAuthProvider("github", config)
+    state = "one"
+    auth_url = provider.set_scopes(["email", "openid"]).get_auth_url(state)
+    parts = list(parse.urlparse(auth_url))
+    query = dict(parse.parse_qsl(parts[4]))
+
+    if query.get("client_id") != "client_id_1":
+        raise AssertionError
+
+
+@patch("python_socialite.drivers.github.requests")
+def test_get_user(mock_requests):
+    mock_requests.get.return_value.ok = True
+    mock_requests.get.return_value.json.return_value = {
+        "id": "103",
+        "name": "John Doe",
+        "login": "johndoe",
+        "email": None,
+    }
+    provider = OAuthProvider("github", config)
+    user = provider.get_user("xxxxxx")
+
+    if user.get("provider") != "github":
+        raise AssertionError
+
+    if user.get("id") != "103":
+        raise AssertionError
+
+    if user.get("username") != "johndoe":
+        raise AssertionError
+
+
+@patch("python_socialite.drivers.github.requests")
+def test_get_token_error(mock_requests):
+    mock_requests.get.return_value.ok = True
+    mock_requests.get.return_value.json.return_value = {
+        "error": "Bad verification code"
+    }
+    provider = OAuthProvider("github", config)
+    state = "one"
+
+    with pytest.raises(BadVerification):
+        provider.get_token("xxxxxx", state)
